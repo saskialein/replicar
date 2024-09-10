dc642741eec084f001bedceb30b0aa54f36b338b
Saskia Leinberger <66111428+saskialein@users.noreply.github.com>
2024-09-10 12:13:15 +1200

    Create sign up and sign in views

--- types/type.d.ts
+++ types/type.d.ts
@@ -7,3 +7,13 @@ declare interface ButtonProps extends TouchableOpacityProps {
   IconRight?: React.ComponentType<any>;
   className?: string;
 }
+declare interface InputFieldProps extends TextInputProps {
+  label: string;
+  icon?: any;
+  secureTextEntry?: boolean;
+  labelStyle?: string;
+  containerStyle?: string;
+  inputStyle?: string;
+  iconStyle?: string;
+  className?: string;
+}
